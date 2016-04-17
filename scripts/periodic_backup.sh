827dfbd07c2df48f1940805530f979174920f907
RaymondKlass <raymond@klassphoto.com>
2016-04-17 04:16:22 -0400

    have backups in outer folder

--- scripts/periodic_backup.sh
+++ scripts/periodic_backup.sh
@@ -8,6 +8,7 @@ export AWS_SECRET_ACCESS_KEY=<SECRET_KEY>
 export AWS_DEFAULT_REGION=us-east-1
 
 mkdir -p /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso_backups
+mkdir -p /usr/local/virtuoso-opensource/var/lib/virtuoso/db/virtuoso_backups/backups
 
 cd /usr/local/virtuoso-opensource/var/lib/virtuoso/db
 
@@ -17,7 +18,7 @@ mkdir -p virtuoso_backups/$NOW
 # this would be the virtuoso backup method...
 isql-v EXEC="dump_one_graph('http://dextro.co', './backup', 10000000);" -U <USER> -P <PASSWORD>
 
-mv backup* virtuoso_backups/${NOW}
+mv backup* virtuoso_backups/backups/${NOW}
 
 cd /usr/local/virtuoso-opensource/var/lib/virtuoso/db
 aws s3 sync virtuoso_backups s3://dextro-alexandria
